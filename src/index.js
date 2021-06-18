1c6fa85fce2071c650f729f94249ee263fbca1a8
exit <zrmartinezg@gmail.com>
2021-06-17 20:18:59 -0500

    Add calculation functionality, error alert

--- src/index.js
+++ src/index.js
@@ -5,3 +5,47 @@ import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
 import './style.css'
 
+
+const loanForm = document.getElementById("loan-form");
+const  amount = document.getElementById("amount");
+const interest = document.getElementById("interest");
+const years = document.getElementById("years");
+const monthlyPayment = document.getElementById("monthly-payment");
+const totalPayment = document.getElementById("total-payment");
+const totalinterest = document.getElementById("total-interest");
+const principalContainer = document.querySelector(".principal-container");
+
+
+const error = () =>{
+  const errorDiv =  document.createElement("div");
+  errorDiv.className = "alert alert-danger";
+  const p = document.createElement("p");
+  p.innerText = "Please introduce the data again";
+  errorDiv.appendChild(p);
+  principalContainer.insertBefore(errorDiv, loanForm);
+  setTimeout(()=>{
+    errorDiv.remove();
+  }, 2000)
+}
+
+
+
+loanForm.addEventListener("submit", (e)=> {
+  e.preventDefault();
+  const amoutValue = amount.value;
+  const interestCalculation = parseFloat(interest.value)/100/12;
+  const paymentsCalculation = (parseFloat(years.value))* 12;
+
+  const x = Math.pow(1 + interestCalculation, paymentsCalculation);
+  const month = (amoutValue * x * interestCalculation)/ (x - 1);
+
+  if(isFinite(month)){
+    monthlyPayment.value = month.toFixed(2);
+    totalPayment.value = (month * paymentsCalculation).toFixed(2);
+    totalinterest.value = ((month * paymentsCalculation) - amoutValue).toFixed(2);
+  } else {
+    error();    
+  }
+
+})
+
